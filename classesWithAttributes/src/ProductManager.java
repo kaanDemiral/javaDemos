ffd10a415f6b4f9f8b40a05bb220f4aae85dbfb9
kaanDemiral <75839912+kaanDemiral@users.noreply.github.com>
2022-09-28 19:41:21 +0300

    Java bootcamp 2nd week

---
+++ classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager {
+    public void Add(Product product) {
+        //JDBC
+
+    }
+
+    public void Add2(int id, String name, String description, int stockAmound, double price) {
+
+    }
+}
